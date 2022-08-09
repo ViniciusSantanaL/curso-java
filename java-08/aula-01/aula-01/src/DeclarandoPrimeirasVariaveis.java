14ff216c50656c58d3f9f443feea11f9eda14495
Vinicius Santana <viniciussantanaleao@gmail.com>
2022-08-09 18:00:58 -0300

    Códigos da primeira aula

---
+++ java-08/aula-01/aula-01/src/DeclarandoPrimeirasVariaveis.java
@@ -0,0 +1,9 @@
+
+public class DeclarandoPrimeirasVariaveis {
+	public static void main(String args[]) {
+		int idade;
+		idade = 21;
+		int fulano = 21;
+		System.out.println("Minha idade eh: " +  idade + " anos");
+	}
+}
---
+++ java-08/aula-01/primeiro-projeto-java/src/Curso.java
@@ -0,0 +1,6 @@
+
+public class Curso {
+	public static void main(String args[]) {
+		System.out.println("Ola Mundo!");
+	}
+}
